62b8f431517262423dcf521a4175260b4bea8f05
Stas I <ivanaevskystas@gmail.com>
2023-07-07 11:35:47 +0000

    add ec2 main.tf

---
+++ terraform/modules/ec2/variables.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-central-1"
+}
+
+variable "instance_name" {
+  type = string
+  default = "Jenkins-agent"
+}
+
+variable "ami_id" {
+  type = string
+  default = ""
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.small"
+}
+
+variable "key_name" {
+  type = string
+  default = "WorkFrankfurt"
+}
+
+variable "security_group_ids" {
+  type    = list(string)
+  default = [""]
+}
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "subnet_ids" {
+  type    = list(string)
+  default = [","]
+}
